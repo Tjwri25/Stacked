152753928af1d28f246e6c4b02e94ea6188a439d
Taylor Wright <tjwri25@gmail.com>
2021-05-19 19:03:57 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Greet from "./components/Greet";
 import Header from "./components/Header"
+import TemporaryDrawer from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Greet />
+      <TemporaryDrawer />
     </div>
   );
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-      <img src="https://user-images.githubusercontent.com/74884495/118894209-0bbbfb80-b8c1-11eb-8e5e-e09bddfefc2a.png" alt="" width="300" height="100" class="d-inline-block align-text-top"></img>
+      <img src="https://user-images.githubusercontent.com/74884495/118894209-0bbbfb80-b8c1-11eb-8e5e-e09bddfefc2a.png" alt="" width="300" height="100" className="d-inline-block align-text-top"></img>
       </a>
     </nav>
   );
