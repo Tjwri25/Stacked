df1ea578256e2aa9802d03e8f87687981e42486a
Taylor Wright <tjwri25@gmail.com>
2021-05-20 00:55:09 -0600

    hamburger menu

--- client/src/components/Nav/closeNav.js
+++ client/src/components/Nav/closeNav.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 
 function CloseNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0px";
   }
 
   export default CloseNav
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 import OpenNav from "./openNav";
-import CloseNav from "./closeNav"
-import "./style.css"
-
+import CloseNav from "./closeNav";
+import "./style.css";
 
 function Nav() {
-    return (
-<div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-      <img src="https://user-images.githubusercontent.com/74884495/118894209-0bbbfb80-b8c1-11eb-8e5e-e09bddfefc2a.png" alt="" width="300" height="100" className="d-inline-block align-text-top"></img>
-      </a>
-    </nav>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">
+          <img
+            src="https://user-images.githubusercontent.com/74884495/118894209-0bbbfb80-b8c1-11eb-8e5e-e09bddfefc2a.png"
+            alt=""
+            width="300"
+            height="100"
+            className="d-inline-block align-text-top"
+          ></img>
+        </a>
+      </nav>
 
+      <div>
+        <div id="mySidenav" className="sidenav">
+          <a href="javascript:void(0)" className="closebtn" onClick={CloseNav}>
+            &times;
+          </a>
+          <a href="#">Search</a>
+          <a href="#">Articles</a>
+          <a href="#">Community Board</a>
+          <a href="#">Log In</a>
+        </div>
 
-
-
-        <div>
-<div id="mySidenav" className="sidenav">
-  <a href="javascript:void(0)" className="closebtn" onClick={CloseNav}>&times;</a>
-  <a href="#">Search</a>
-  <a href="#">Articles</a>
-  <a href="#">Community Board</a>
-  <a href="#">Log In</a>
-
-</div>
-
-<span onClick={OpenNav}>open</span>
-</div>
-
-
-</div>
-
-)
+        <span className="openbtn" onClick={OpenNav}>
+          <div className="hmbgrMenu"></div>
+          <div className="hmbgrMenu"></div>
+          <div className="hmbgrMenu"></div>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
