2ccc5d67a7a35260050b21cf0ce2c5e9d526c1c6
MK <mihaelak08@gmail.com>
2021-06-12 12:51:59 -0700

    more data

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,20 +10,27 @@ const actData = [
     introduced: ["Week 2, Activity 1, Line 3"],
 
     used: [
-      "Week: 2, Activity: 7, Line: 6, 9, 10  |" ,
-      "Week: 7, Activity: 2, Folder: assets/css/style.css, Line: 3  |",
-      "Week: 8, Activity: 6, Folder: assets/css/style.css Line: 2  |",
+      "Week: 2, Activity: 7, Line: 6, 9, 10  |",
+      "Week: 7, Activity: 2, Folder: assets/css/style.css, Line: 3",
+      "Week: 8, Activity: 6, Folder: assets/css/style.css Line: 2",
     ],
   },
   {
     title: "for loop",
     docs: ["https://www.w3schools.com/forloop/"],
-    introduced: ["Week 6, Activity 5, Line 7"],
+    introduced: ["Week 3, Activity 11, Line 2"],
+
+    used: ["Week: 3, Activity: 13, Line: 11"],
+  },
+  {
+    title: "components",
+    docs: ["https://www.w3schools.com/react/react_components.asp"],
+    introduced: ["Week 19, Activity 3, Folder: Solved/components/HelloDiv.js"],
 
     used: [
-      "Week: 6, Activity: 9, Line: 3,",
-      "Week: 6, Activity: 3, Folder: assets/css/style.css, Line: 7",
-      
+      "Week: 19, Activity: 5, Folder: Solved/App.js, Solved/components/HelloBootstrap.js, Line: 1,",
+      "Week: 19, Activity: 9, Folder: Solved/components, Solved/App.js, Line 9, 10, 11",
+      "Week 19, Activity: ",
     ],
   },
 ];
@@ -40,4 +47,4 @@ db.activityData
     process.exit(1);
   });
 
-  module.exports = actData;
+module.exports = actData;
